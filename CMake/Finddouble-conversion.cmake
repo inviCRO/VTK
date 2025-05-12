8cd0253b07195a9702f6282aa9c23077da747a11
jpieszala <jpieszala@invicro.com>
2025-05-12 15:14:06 -0400

    This was always finding the debug version and causing that iterator debug mismatch linking issue in VQ release mode. I'd like to try our build process out after Tim's changes to see if somehow we are resolving our deps better. Note: have a bunch of "debug" libs trying to link in VQ release builds.

--- CMake/Finddouble-conversion.cmake
+++ CMake/Finddouble-conversion.cmake
@@ -1,29 +1,46 @@
 find_path(double-conversion_INCLUDE_DIR
-  NAMES
-    double-conversion.h
-  PATH_SUFFIXES
-    double-conversion
-  DOC "double-conversion include directory")
-mark_as_advanced(double-conversion_INCLUDE_DIR)
+  NAMES double-conversion.h
+  PATH_SUFFIXES double-conversion
+  DOC "double-conversion include directory"
+)
 
-find_library(double-conversion_LIBRARY
-  NAMES
-    double-conversion
-  DOC "double-conversion library")
-mark_as_advanced(double-conversion_LIBRARY)
+find_library(double-conversion_LIBRARY_RELEASE
+  NAMES double-conversion
+  PATH_SUFFIXES lib
+)
+
+find_library(double-conversion_LIBRARY_DEBUG
+  NAMES double-conversion
+  PATH_SUFFIXES debug/lib
+)
+
+include(SelectLibraryConfigurations)
+select_library_configurations(double-conversion)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(double-conversion
-  REQUIRED_VARS double-conversion_LIBRARY double-conversion_INCLUDE_DIR)
+  REQUIRED_VARS double-conversion_LIBRARY double-conversion_INCLUDE_DIR
+)
 
 if (double-conversion_FOUND)
   set(double-conversion_INCLUDE_DIRS "${double-conversion_INCLUDE_DIR}")
   set(double-conversion_LIBRARIES "${double-conversion_LIBRARY}")
 
-  if (NOT TARGET double-conversion::double-conversion)
+  if (NOT TARGET double-conversion::double-conversion AND double-conversion_LIBRARY)
     add_library(double-conversion::double-conversion UNKNOWN IMPORTED)
+
     set_target_properties(double-conversion::double-conversion PROPERTIES
-      IMPORTED_LOCATION "${double-conversion_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${double-conversion_INCLUDE_DIR}")
-  endif ()
-endif ()
+      INTERFACE_INCLUDE_DIRECTORIES "${double-conversion_INCLUDE_DIR}"
+    )
+
+    if (double-conversion_LIBRARY_RELEASE)
+      set_property(TARGET double-conversion::double-conversion PROPERTY
+        IMPORTED_LOCATION_RELEASE "${double-conversion_LIBRARY_RELEASE}")
+    endif()
+
+    if (double-conversion_LIBRARY_DEBUG)
+      set_property(TARGET double-conversion::double-conversion PROPERTY
+        IMPORTED_LOCATION_DEBUG "${double-conversion_LIBRARY_DEBUG}")
+    endif()
+  endif()
+endif()
